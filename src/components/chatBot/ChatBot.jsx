83631e07b9659d25dc82d38fd8a35a7686af65e0
markitos <markitos@pop-os.localdomain>
2024-02-27 12:44:10 -0300

    cambio

---
+++ src/components/chatBot/ChatBot.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import './ChatBot.css'; // Asegúrate de tener este archivo CSS en tu proyecto
+// import bot png
+import bot from '../../assets/bot.png';
+
+// Datos de las áreas del derecho
+const derechoData = [
+    {
+      area: "Derecho Familiar",
+      contenido: "Sucesiones - Conflictos familiares - Cuota alimentaria - Comunicación con los hijos - Divorcio",
+      abogado: "Abogado Ramiro Mateo",
+      numero: "+54 11 4743-8758",
+      palabra_clave: "familia"
+    },
+    {
+        area: "Derecho Laboral",
+        contenido: "Despidos - Accidentes de trabajo - Trabajo en condiciones insalubres",
+        abogado: "Abogada Hermana Ramiro",
+        numero: "+54 11 3341-5563",
+        palabra_clave: "laboral"
+    },
+    {
+      area: "Derecho penal",
+      contenido: "Robo - Homicidio - Lesiones - Abuso sexual",
+      abogado: "Sr Padre Ramiro",
+      numero: "+54 11 2231-4551",
+        palabra_clave: "penal"
+    },
+    {
+      area: "Derecho Civil",
+      contenido: "Contratos - Daños y perjuicios - Derechos reales - Derecho de familia",
+      abogado: "Prima Ramiro",
+      numero: "+54 11 4415-5112",
+        palabra_clave: "civil"
+    }
+  ]
+  
+function ChatBot() {
+  const [chatOpen, setChatOpen] = useState(true); // Cambiado a true para que el chat esté abierto por defecto
+  const [messages, setMessages] = useState([{text: "¡Hola! ¿Que tipo de derecho le interesa encontrar?", from: "bot"}]);
+
+  const mensajePredeterminado = `Parece que no puedo encontrar una respuesta específica basada en tu mensaje. 
+  Aquí tienes una lista de las áreas de derecho en las que puedo asistirte:\n\n- Derecho Familiar, Derecho Laboral, 
+  Derecho Penal, Derecho Civil\n\nPor favor, escribe el área de derecho en la que necesitas asistencia.`; 
+
+
+  // Función para simular el envío de un mensaje
+  const sendMessage = (event) => {
+    event.preventDefault();
+    const messageText = event.target.message.value.trim();
+    if (messageText) {
+      setMessages([...messages, {text: messageText, from: "user"}]);
+      // Lógica para identificar palabras clave y generar respuesta
+      const respuesta = derechoData.find(d => messageText.toLowerCase().includes(d.palabra_clave));
+      if (respuesta) {
+        const respuestaBot = `Nuestro abogado especialista en ${respuesta.area} es ${respuesta.abogado}. Puede contactarlo al ${respuesta.numero}`;
+        setMessages(messages => [...messages, {text: respuestaBot, from: "bot"}]);
+      } else {
+        // Si no encuentra una coincidencia, envía el mensaje predeterminado
+        setMessages(messages => [...messages, {text: mensajePredeterminado, from: "bot"}]);
+      }
+
+      event.target.reset(); // Limpiar el campo después de enviar
+    }
+  };
+
+  return (
+    <div className={`chat-bot ${chatOpen ? 'open' : 'closed'}`}>
+      <div className="chat-header" onClick={() => setChatOpen(!chatOpen)}>
+        <span> 
+                Bot Estudio Mateo
+        </span>
+
+        <img src={bot} alt="Bot" className={`bot-icon ${chatOpen ? '' : 'bot-closed'}`} />
+
+      </div>
+      <div className="messages">
+        {messages.map((msg, index) => (
+          <div key={index} className={`message ${msg.from}`}>
+            {msg.text}
+          </div>
+        ))}
+      </div>
+      {chatOpen && (
+        <form onSubmit={sendMessage} className="message-form">
+          <input type="text" name="message" placeholder="Escribe tu mensaje aquí..." />
+          <button type="submit">Enviar</button>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default ChatBot;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import styles from "./home.module.css";
 import { VscMail } from "react-icons/vsc";
 import { FaWhatsapp } from "react-icons/fa";
 import { IoLogoInstagram } from "react-icons/io5";
+import ChatBot from "../../components/chatBot/ChatBot";
 function Home() {
   const iconSize = 33;
   const headerHeight = 100;
@@ -83,6 +84,7 @@ function Home() {
           <div></div>
         </section>
       </div>
+      <ChatBot />
     </>
   );
 }
