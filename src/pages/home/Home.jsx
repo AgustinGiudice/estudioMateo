200abf2e2c0848f625f76c730987cb32816c72e9
markitos <markitos@pop-os.localdomain>
2024-03-08 13:55:08 -0300

    cambio marcos

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -130,13 +130,13 @@ function Home() {
         <div
           style={{
             position: 'fixed',
-            bottom: '30vh',
+            bottom: '10vh',
             right: '2rem',
-            cursor: 'pointer'
+            cursor: 'pointer',
           }}
           onClick={handleChatBotToggle}
         >
-          <BsChatDots size={iconSize * 2} /> {/* Icono de chat más grande */}
+          <BsChatDots size={iconSize * 1.8} className={styles.chatBotIcon} />
         </div>
       )}
     </>
